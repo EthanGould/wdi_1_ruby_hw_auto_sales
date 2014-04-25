20911349e611e9ec6b4bea956a2d2217bbc866ca
Ethan Gould <ethan.g.907@gmail.com>
2014-04-24 21:32:35 -0400

    added a currenct vlaue price adjustment if damage is present on the car

--- lib/used_car.rb
+++ lib/used_car.rb
@@ -2,8 +2,15 @@ class UsedCar < Car
 
   attr_accessor :damage
 
-  def intialize(make, model, year, damage)
+  def initialize(make, model, year, damage)
     @damage = damage
     super(make, model, year)
+  end
+
+  def current_value
+    if damage
+      return damage.cost - super
+    end
+    super
   end
 end
