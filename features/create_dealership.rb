3aef6036b7d349677bfa5ee471411ebb9fac82a4
Ethan Gould <ethan.g.907@gmail.com>
2014-04-27 17:04:01 -0400

    tried to implement the unique id's for each car
    * putting the unique car id's broke my code all over
    * the place so i commented it out...

--- features/create_dealership.rb
+++ features/create_dealership.rb
@@ -2,10 +2,6 @@ require_relative '../lib/car.rb'
 require_relative '../lib/used_car.rb'
 require_relative '../lib/dealership.rb'
 
-require 'pry'
-
 my_dealership = Dealership.new("Ethans Car Lot")
 
 puts "Welcome to #{my_dealership.name}!"
-
-binding.pry
