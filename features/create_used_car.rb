fdfed90cc74df36757f08f4be660a5975ece6276
Ethan Gould <ethan.g.907@gmail.com>
2014-04-27 17:22:31 -0400

    Revert "tried to implement the unique id's for each car"
    This reverts commit 3aef6036b7d349677bfa5ee471411ebb9fac82a4.

--- features/create_used_car.rb
+++ features/create_used_car.rb
@@ -1,6 +1,8 @@
+require 'pry'
 require_relative '../lib/used_car.rb'
 
 used_car = UsedCar.new('Batmobile', 'batwing', '2010', '20_000', '10_000')
 
-puts "The used car is #{used_car.make} #{used_car.model} #{used_car.year}, $#{used_car.msrp}, with #{used_car.milage} miles on it"
+puts "used car is #{used_car.make} #{used_car.model} #{used_car.year}, $#{used_car.msrp}, with #{used_car.milage} miles on it"
 
+binding.pry
